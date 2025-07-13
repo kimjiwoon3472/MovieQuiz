bf96b6f9e28c6463b82aed960d46d4e18d0eed6f
kimjiwoon3472 <25_17@bssm.hs.kr>
2025-07-13 23:10:30 +0900

    kimjiwoon

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const light = document.getElementById("light");
 let visible = true;
 
 setInterval(() => {
-    light.style.opacity = visible ? '0.25' : '1';
+    light.style.opacity = visible ? '0.3' : '1';
     visible = !visible;
     console.log(light.style.opacity);
 }, 2000)
